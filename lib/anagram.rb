074cfd06db609fc90ee0aab6396dc1ee1e08fe4c
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-10-23 12:16:22 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select {|w| w.split("").sort == @word.split("").sort}
+    end
+end
+
+listen= Anagram.new("listen")
+pp listen.match(%w(enlist google inlets banana))
